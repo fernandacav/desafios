443683ce90e996f1d7b5f5ca90549dc08fc359ce
Fernanda de Souza <a.fernandadesouza@gmail.com>
2024-10-28 18:09:52 -0300

    desafio de conversão de csv em json

---
+++ src/pages/CSV2JSON.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { Box, Button, Card, TextareaAutosize, Typography } from "@mui/material";
+
+function Cvs2Json() {
+  const [csvText, setCsvText] = useState<string>("");
+  const [jsonOutput, setJsonOutput] = useState<string>("");
+
+  const handleConvert = () => {
+    const lines = csvText.split("\n");
+    const headers = lines[0].split(",");
+    const jsonArray = [];
+    console.log("headers", headers);
+    for (let i = 1; i < lines.length; i++) {
+      const currentLine = lines[i];
+      if (currentLine === "") continue;
+      const values = currentLine.split(",");
+      const jsonObject: any = {};
+
+      headers.forEach((header, index) => {
+        jsonObject[header] = values[index]?.trim();
+      });
+
+      jsonArray.push(jsonObject);
+    }
+    console.log(jsonArray);
+    setJsonOutput(JSON.stringify(jsonArray, null, 2));
+  };
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#afe0c0",
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Typography sx={{ marginTop: "60px", fontSize: "48px" }}>
+        Converter CSV em JSON
+      </Typography>
+      <Box
+        sx={{
+          marginTop: 20,
+          marginBottom: 8,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Box sx={{ mx: 10 }}>
+          <TextareaAutosize
+            minRows={8}
+            value={csvText}
+            onChange={(e) => setCsvText(e.target.value)}
+          ></TextareaAutosize>
+        </Box>
+        <Card sx={{ width: "350px" }}>{jsonOutput}</Card>
+      </Box>
+      <Button onClick={handleConvert}>CSV em JSON</Button>
+    </Box>
+  );
+}
+
+export default Cvs2Json;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,6 +3,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import Bin2dec from "../pages/Bin2dec";
 import BorderRadius from "../pages/BorderRadius/BorderRadius";
+import Cvs2Json from "../pages/CSV2JSON";
+import Calculator from "../pages/Calculator";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +14,14 @@ const router = createBrowserRouter([
   {
     path: "/borderradius",
     element: <BorderRadius />,
+  },
+  {
+    path: "/csv2json",
+    element: <Cvs2Json />,
+  },
+  {
+    path: "/calculator",
+    element: <Calculator />,
   },
 ]);
 
